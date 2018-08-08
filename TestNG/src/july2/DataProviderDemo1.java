edeebb8b1b93c47bfedb654fb75034c5b2194b2e
Seleniumlearning0806 <seleniumlearning0806@gmail.com>
2018-08-08 22:49:49 +0530

    Test123

--- TestNG/src/july2/DataProviderDemo1.java
+++ TestNG/src/july2/DataProviderDemo1.java
@@ -3,7 +3,7 @@ package july2;
 import org.testng.annotations.DataProvider;
 
 public class DataProviderDemo1 {
-	
+	//This is DataProvider Demo Sample
 	@DataProvider(name="selenium")
 	public static Object[][] dataDemo(){
 		Object[][] data=new Object[2][1];
